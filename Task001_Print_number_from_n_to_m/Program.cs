2d484f1600ae645de5264e251d56f28faae35880
Ekaterina <tiurina.kate@gmail.com>
2022-06-26 19:06:26 +0300

    Task 001 Print number from m to n

---
+++ Task001_Print_number_from_n_to_m/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+string NumbersRec(int m, int n)
+{
+if (m <= n) return NumbersRec(m + 1, n) + $"{m} ";
+else return String.Empty;
+}
+Console.WriteLine(NumbersRec(12, 55));
