f2d7c669c341f84f15f5f86b9b897dce3a599753
Ekaterina <tiurina.kate@gmail.com>
2022-06-26 19:14:37 +0300

    Task002 sum of numbers from n to m

---
+++ Task002_Sum_of_numbers_from_n_to_m/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumOfNumbers (int m, int n)
+{
+    if (m==n) return n;    
+    else return n+SumOfNumbers(m,n-1);    
+}
+Console.WriteLine(SumOfNumbers(1,10));
